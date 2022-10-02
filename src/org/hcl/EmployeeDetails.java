3c74ed78bcfc097fa0b118b9b4f3f9efeb581ac4
ranjanisenthil <ranjanisei2012@gmail.com>
2022-10-02 22:05:04 +0530

    ranjanisenthil

---
+++ src/org/hcl/EmployeeDetails.java
@@ -0,0 +1,46 @@
+//package
+package org.hcl;
+
+//class
+public class EmployeeDetails {
+	
+//Method creation
+//Method name ctrl+space+enter
+//Method1
+	private void employeeName() {
+		
+	System.out.println("Employee Name: Ragul");
+}
+
+//Method2
+	private void employeeId() {
+	//sysout ctrl+space	
+		System.out.println("Employee Id: 2078");
+		
+	}
+//Main method creation
+	public static void main(String[] args) {
+//Object creation
+//ClassName objectName = new ClassName();
+		EmployeeDetails e = new EmployeeDetails();
+		
+//Method call
+//objectName.methodName();
+		e.employeeName();
+		e.employeeId();
+		
+	}
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
